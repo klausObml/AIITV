8d42d70efe1208b78d78a39d421fe7e7d61ce79e
Klaus <klaus.obermueller@liwest.at>
2020-01-22 09:58:55 +0100

    first program release

--- Python_01.py
+++ Python_01.py
@@ -1,3 +1,33 @@
 #!/usr/bin/python
 
-print("HelloWorld")
+import types
+
+"""
+Zahlen eingeben, mit q beenden, Mittelwert berechnen
+"""
+
+num = []
+average = 0
+
+
+def averageList (myList):
+    
+
+
+if __name__ == "__main__":
+    print("Bitte Zahlen eingeben und nach letzter Zahl mit 'q' bestätigen.")
+    while True:
+        x = input()
+        if x == 'q':
+            print("letzte Eingabe erfolgt.")
+            for i in num:
+                average += i
+            average /= len(num)
+            print(average)
+
+        else:
+            try:
+                num.append(int(x))
+            except:
+                print("Falsche Eingabe.")
+        
